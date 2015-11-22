78ecce3a6399cd1521cd06442e00b47953859dfd
test <roolues@yahoo.co.jp>
2015-11-22 12:27:47 +0900

    newをコントローラに登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,14 @@
 class ItemsController < ApplicationController
   def show
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:id]) #findは何行目のデータを持ってきて！という命令
   end
 
   def index
     @items = Item.all
+  end
+
+  def new
+    @item = Item.new
   end
 
 end
