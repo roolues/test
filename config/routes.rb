836edb4813b95b19cef1ef73e913449214733001
test <roolues@yahoo.co.jp>
2015-11-22 12:35:02 +0900

    ルーティングにcreaiteを設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get '/items' => 'items#index'
 
+  get '/items/' => 'items$create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
